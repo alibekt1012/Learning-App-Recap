703606bf7887b2c0b6082228d21ffcf57b7ffd10
alibekt1012 <alibekt1012@gmail.com>
2023-03-16 19:01:52 +0600

    Initial Commit

---
+++ Learning App (Recap)/Learning_App__Recap_App.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_App__Recap_App.swift
+//  Learning App (Recap)
+//
+//  Created by Almat Alibekov on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Learning_App__Recap_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
